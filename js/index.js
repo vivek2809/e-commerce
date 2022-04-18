366106c02ad544d6cefef8682df6e6226e904963
vivek <vivekpatel07103@gmail.com>
2022-04-18 09:53:48 +0530

    product carousel done

--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
-let productAccordian = document.getElementById('product_data');
+//for product carousel
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
+
+let productAccordian = document.getElementById('autoWidth');
 
 const xhr = new XMLHttpRequest();
-xhr.open('GET',`http://fatafatbooks.com/api/books/gethomebooks`,true);
+xhr.open('GET', `http://fatafatbooks.com/api/books/gethomebooks`, true);
 
-xhr.onload = function(){
-    if(this.status === 200){
+xhr.onload = function () {
+    if (this.status === 200) {
         let json = JSON.parse(this.responseText);
         let latest = json.latest;
         console.log(latest);
         let productHtml = "";
-        latest.forEach(function(element){
-            let products = `<div class="col product_item">
-                                    <img src="${element["thumbnail"]}" alt="product" class="img-fluid">
-                                        <div class="product_detail">
-                                            <h4>${element["bookName"]}</h4>
-                                            <h5>${element["booksId"]}</h5>
-                                        </div>
-                                </div>`
-                                console.log(element.authorName);
+        latest.forEach(function (element) {
+
+            let products = `<li class="item-a">
+                    <div class="box">
+                        <div class="slid-img">
+                            <img src="${element["thumbnail"]}" alt="product img">
+                        </div>
+                        <div class="detail-box">
+                            <h3>Name:${element["bookName"]}</h3>
+                            <h3>Id:${element["booksId"]}</h3>
+                        </div>
+                    </div>
+                </li>`
+            console.log(element.authorName);
             productHtml += products;
         });
         productAccordian.innerHTML = productHtml;
     }
-    else{
+    else {
         console.log("Some error occured");
     }
 }
